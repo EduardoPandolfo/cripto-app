c0f0756647a8603be067a393b99d35d94766c130
EduardoPandolfo <ekpandolfo@gmail.com>
2023-08-16 14:54:30 -0300

    Project created, h2 configuring, table coin created

---
+++ src/main/java/com/eduardokp/criptoappsecurity/CriptoAppSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.eduardokp.criptoappsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoAppSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriptoAppSecurityApplication.class, args);
+    }
+
+}
